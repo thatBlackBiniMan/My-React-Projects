a27e84ace9096372d467c0e7449c5bd85a34aa35
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-05 22:57:48 +0100

    my react project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component{
+ state = {advice: ''};
+componentDidMount(){
+  this.fetchAdvice();
+}
+
+fetchAdvice = ()=>{
+  axios.get('https://api.adviceslip.com/advice')
+  .then((response)=>{
+    const {advice} = response.data.slip;
+    console.log(advice);
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
+}
+
+  render(){
+  return(
+    <h1>App</h1>
+  );
+ }
+}  
+
+   
+export default App;
